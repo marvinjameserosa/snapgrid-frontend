114061387783bd7f931ea505a10039525053a06c
BeginnerBoi1 <santoschinkit@gmail.com>
2025-12-13 23:05:46 +0800

    refactor: move Home component to new GridLayoutSelection page and remove from Home page

--- snapgrid/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Image from "next/image";
-import GridLayout from "@/components/sections/Grid-Layout";
-export default function Home() {
-  return (
-    <>
-    <GridLayout />
-    </>
-  );
-}
